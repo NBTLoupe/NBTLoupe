c9622e3b04e0b325d8f7c9bc9b8a7fdd6a7bde91
MallardLuna <142849583+MallardLuna@users.noreply.github.com>
2026-09-05 06:30:05 -0300

    Move everything to modern extension methods, for consistency reasons.

--- NBTLoupe/Core/TreeNodes/NodeRegionSearcher.cs
+++ NBTLoupe/Core/TreeNodes/NodeRegionSearcher.cs
@@ -5,87 +5,90 @@ namespace NBTLoupe.Core.TreeNodes;
 
 internal static class NodeRegionSearcher
 {
-    // This function allows us to Search for a specific Regional TreeNode.
-    internal static async Task<TreeNode?> SearchRegionAsync(this TreeNode node, int regionX, int regionZ,
-        int localChunkX, int localChunkZ)
+    extension(TreeNode node)
     {
-        // Starting at our root DataNode...
-        switch (node.DataNode)
+        // This function allows us to Search for a specific Regional TreeNode.
+        internal async Task<TreeNode?> SearchRegionAsync(int regionX, int regionZ,
+            int localChunkX, int localChunkZ)
         {
-            // ...if it's a Directory...
-            case DirectoryDataNode:
+            // Starting at our root DataNode...
+            switch (node.DataNode)
             {
-                switch (node.SubNodes)
+                // ...if it's a Directory...
+                case DirectoryDataNode:
                 {
-                    // Immediately return if it doesn't have SubNodes.
-                    case null:
+                    switch (node.SubNodes)
+                    {
+                        // Immediately return if it doesn't have SubNodes.
+                        case null:
+                            return null;
+
+                        // And, if we didn't yet, we lazy-load it.
+                        case [{ IsPlaceholder: true }]:
+                            await node.LazyLoadAsync();
+                            break;
+                    }
+
+                    // Afterwards, we loop through its children...
+                    foreach (var subNode in node.SubNodes)
+                    {
+                        // ...to keep searching on them...
+                        var resultNode = await subNode.SearchRegionAsync(regionX, regionZ, localChunkX, localChunkZ);
+
+                        // ...until one of them is the result.
+                        if (resultNode != null) return resultNode;
+                    }
+
+                    break;
+                }
+
+                // ...if it's a RegionFile...
+                case RegionFileDataNode regionNode:
+                {
+                    // ...if we aren't in the right Region, we immediately return.
+                    if (!RegionFileDataNode.RegionCoordinates(regionNode.NodePathName, out var rx, out var rz))
+                        return null;
+                    if (rx != regionX || rz != regionZ)
                         return null;
 
-                    // And, if we didn't yet, we lazy-load it.
-                    case [{ IsPlaceholder: true }]:
-                        await node.LazyLoadAsync();
-                        break;
+                    // But if it is the right Region...
+                    switch (node.SubNodes)
+                    {
+                        // Immediately return if it doesn't have SubNodes.
+                        case null:
+                            return null;
+
+                        // And, if we didn't yet, we lazy-load it.
+                        case [{ IsPlaceholder: true }]:
+                            await node.LazyLoadAsync();
+                            break;
+                    }
+
+                    // Afterwards, we loop through its children...
+                    foreach (var subNode in node.SubNodes)
+                    {
+                        // ...to keep searching on them...
+                        var resultNode = await subNode.SearchRegionAsync(regionX, regionZ, localChunkX, localChunkZ);
+
+                        // ...until one of them is the result.
+                        if (resultNode != null) return resultNode;
+                    }
+
+                    break;
                 }
 
-                // Afterwards, we loop through its children...
-                foreach (var subNode in node.SubNodes)
-                {
-                    // ...to keep searching on them...
-                    var resultNode = await subNode.SearchRegionAsync(regionX, regionZ, localChunkX, localChunkZ);
+                // ...if it's a Chunk...
+                // ...it either isn't the right one...
+                case RegionChunkDataNode chunkNode when chunkNode.X != localChunkX || chunkNode.Z != localChunkZ:
+                    break;
 
-                    // ...until one of them is the result.
-                    if (resultNode != null) return resultNode;
-                }
-
-                break;
+                // ...or we found it! In which case, we return it.
+                case RegionChunkDataNode:
+                    return node;
             }
 
-            // ...if it's a RegionFile...
-            case RegionFileDataNode regionNode:
-            {
-                // ...if we aren't in the right Region, we immediately return.
-                if (!RegionFileDataNode.RegionCoordinates(regionNode.NodePathName, out var rx, out var rz))
-                    return null;
-                if (rx != regionX || rz != regionZ)
-                    return null;
-
-                // But if it is the right Region...
-                switch (node.SubNodes)
-                {
-                    // Immediately return if it doesn't have SubNodes.
-                    case null:
-                        return null;
-
-                    // And, if we didn't yet, we lazy-load it.
-                    case [{ IsPlaceholder: true }]:
-                        await node.LazyLoadAsync();
-                        break;
-                }
-
-                // Afterwards, we loop through its children...
-                foreach (var subNode in node.SubNodes)
-                {
-                    // ...to keep searching on them...
-                    var resultNode = await subNode.SearchRegionAsync(regionX, regionZ, localChunkX, localChunkZ);
-
-                    // ...until one of them is the result.
-                    if (resultNode != null) return resultNode;
-                }
-
-                break;
-            }
-
-            // ...if it's a Chunk...
-            // ...it either isn't the right one...
-            case RegionChunkDataNode chunkNode when chunkNode.X != localChunkX || chunkNode.Z != localChunkZ:
-                break;
-
-            // ...or we found it! In which case, we return it.
-            case RegionChunkDataNode:
-                return node;
+            // And if we didn't find anything, we just return that.
+            return null;
         }
-
-        // And if we didn't find anything, we just return that.
-        return null;
     }
 }
